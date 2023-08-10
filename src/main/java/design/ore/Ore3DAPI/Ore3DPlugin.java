0d8791e25863c0d7a8ec9c845073ad73b5738a2b
Jacob Batista <jacobaccio@gmail.com>
2023-08-10 15:33:07 -0600

    Logger Functionality

--- src/main/java/design/ore/Ore3DAPI/Ore3DPlugin.java
+++ src/main/java/design/ore/Ore3DAPI/Ore3DPlugin.java
@@ -1,14 +1,22 @@
 package design.ore.Ore3DAPI;
 
 import org.pf4j.Plugin;
+import org.pf4j.RuntimeMode;
+import org.slf4j.Logger;
 
 public abstract class Ore3DPlugin extends Plugin
 {
 	protected final PluginContext context;
+	
+	private static boolean isDebug;
+	public static boolean isDebug() { return isDebug; }
+	protected static Logger LOG;
 
     protected Ore3DPlugin(PluginContext context)
     {
         super();
+        LOG = context.getLogger();
         this.context = context;
+		isDebug = context.getRuntimeMode() == RuntimeMode.DEVELOPMENT;
     }
 }
--- src/main/java/design/ore/Ore3DAPI/PluginContext.java
+++ src/main/java/design/ore/Ore3DAPI/PluginContext.java
@@ -1,18 +1,20 @@
 package design.ore.Ore3DAPI;
 
 import org.pf4j.RuntimeMode;
+import org.slf4j.Logger;
 
 public class PluginContext
 {
 	private final RuntimeMode runtimeMode;
+	private final Logger logger;
 	
-	public PluginContext(RuntimeMode runtimeMode)
+	public PluginContext(RuntimeMode runtimeMode, Logger logger)
 	{
 		this.runtimeMode = runtimeMode;
+		this.logger = logger;
 	}
 	
-	public RuntimeMode getRuntimeMode()
-	{
-		return runtimeMode;
-	}
+	public RuntimeMode getRuntimeMode() { return runtimeMode; }
+	
+	public Logger getLogger() { return logger; }
 }
