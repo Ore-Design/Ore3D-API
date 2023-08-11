f46f1cc666cd8e9cc5d3ca54ba37647a4ecfd995
Jacob Batista <jacobaccio@gmail.com>
2023-08-11 10:44:12 -0600

    Added value mapping

--- src/main/java/design/ore/Ore3DAPI/PluginContext.java
+++ src/main/java/design/ore/Ore3DAPI/PluginContext.java
@@ -1,5 +1,7 @@
 package design.ore.Ore3DAPI;
 
+import java.util.Map;
+
 import org.pf4j.RuntimeMode;
 import org.slf4j.Logger;
 
@@ -8,17 +10,21 @@ public class PluginContext
 	private final RuntimeMode runtimeMode;
 	private final Logger logger;
 	private final boolean debug;
+	private final Map<String, String> valueMap;
 	
-	public PluginContext(RuntimeMode runtimeMode, Logger logger, boolean debug)
+	public PluginContext(RuntimeMode runtimeMode, Logger logger, boolean debug, Map<String, String> valueMap)
 	{
 		this.runtimeMode = runtimeMode;
 		this.logger = logger;
 		this.debug = debug;
+		this.valueMap = valueMap;
 	}
 	
 	public RuntimeMode getRuntimeMode() { return runtimeMode; }
 	
 	public Logger getLogger() { return logger; }
-	
+
 	public boolean isDebug() { return debug; }
+	public Map<String, String> getValueMap() { return valueMap; }
+	public String getValue(String key) { return valueMap.get(key); }
 }
