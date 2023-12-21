829d591ed57dfc90fc27a4c1f429a6f2123466ed
Jacob Batista <jacobaccio@gmail.com>
2023-12-21 09:45:32 -0700

    Added Transaction reference to createDisableBinding()

--- src/main/java/design/ore/Ore3DAPI/DataTypes/Interfaces/CustomButtonReference.java
+++ src/main/java/design/ore/Ore3DAPI/DataTypes/Interfaces/CustomButtonReference.java
@@ -17,6 +17,6 @@ public abstract class CustomButtonReference
 	
 	public abstract Pane generatePopup(Transaction transaction);
 	public abstract void onClick(Transaction transaction);
-	public abstract BooleanBinding createDisableBinding();
+	public abstract BooleanBinding createDisableBinding(Transaction transaction);
 	public abstract Image getButtonIcon();
 }
